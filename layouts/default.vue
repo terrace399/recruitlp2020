4d6a10e1af7c76f006c03fb26b66a55f6e8fa111
andmohiko <thomasito.berryz@gmail.com>
2020-02-04 19:49:47 +0900

    add: heade

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+// import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 html {
