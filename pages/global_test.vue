093cea99aaa32c6557afcece81136bd5dffeb79b
木瓜丸 <boke0@eugensite.com>
2020-02-06 01:58:51 +0900

    add: test scss&vue files

---
+++ pages/global_test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="container">
+    <div id="exaid" class="testcase"></div>
+    <div id="brave" class="testcase"></div>
+    <div id="snipe" class="testcase"></div>
+    <div id="lazer" class="testcase"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.testcase {
+  height: 80px;
+}
+#exaid {
+  background-color: $mighty-action-x;
+}
+#brave {
+  background-color: $tadoru-quest;
+}
+#snipe {
+  background-color: $bang-bang-shooting;
+}
+#lazer {
+  background-color: $bakuso-bike;
+}
+</style>
